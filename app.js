1f6f5869b6d7528a35ea46cb26104be743c6cd1a
ISAACRAMMM <73269421+ISAACRAMMM@users.noreply.github.com>
2023-11-30 00:42:31 -0600

    Update app.js

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const flowBiceps=addKeyword(['biceps', 'bicep', 'rutina de biceps','ejecicios de
 
 const flowAbdomen=addKeyword(['abdomen', 'abs','rutina de abdomen','ejercicios de abdomen'])
 
-const flowPrincipal = addKeyword(['hola', 'buenas', 'rutina', 'me das una rutina'])
+const flowPrincipal = addKeyword(['hola', 'buenas', 'rutina', 'me das una rutina','ahora que sigue','ya acabe', 'que otra cosa hago'])
     .addAnswer('Bien venido!! Soy tu coach virtual Lucas-35')
     .addAnswer([
         'Que vamos a hacer el dia de hoy?',
