7f2d8b007eefa81cd55bfd52d37e205f26a03a06
ISAACRAMMM <73269421+ISAACRAMMM@users.noreply.github.com>
2023-11-30 22:13:00 -0600

    Update app.js

--- app.js
+++ app.js
@@ -171,18 +171,8 @@ const flowAbdomen=addKeyword(['abdomen', 'abs','rutina de abdomen','ejercicios d
 //
 const flowPrincipal = addKeyword(['hola', 'buenas', 'rutina', 'me das una rutina','ahora que sigue','ya acabe', 'que otra cosa hago'])
     .addAnswer('Bien venido!! Soy tu coach virtual Lucas-35')
-    .addAnswer([
-        'Que vamos a hacer el dia de hoy?',
-        '- Pecho',
-        '- Espalda',
-        '- Pierna',
-        '- Gluteo',
-        '- Biceps',
-        '- Triceps',
-        '- Abdomen',
-        '',
-        '_Escribe lo que vas a entrenar hoy_'
-        ],
+    .addAnswer('Que vamos a hacer el dia de hoy?',{
+        buttons:[{body:'Pecho'},{body:'Espalda'},{body:'Pierna'},{body:'Gluteo'},{body:'Biceps'},{body:'Triceps'},{body:'Abdomen'}]},
         null,
         null, 
         [flowSecundario,
